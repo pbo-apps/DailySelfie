8e443a07e1b23cb871557e10aa8aa41453d37fb4
PBO <petebounford@gmail.com>
2016-10-13 09:29:55 +0200

    Put temp file creation where it was supposed to be in the first place

--- app/src/main/java/com/pbo/apps/dailyselfie/MainActivity.java
+++ app/src/main/java/com/pbo/apps/dailyselfie/MainActivity.java
@@ -45,13 +45,6 @@ public class MainActivity extends AppCompatActivity {
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             // Create the File where the photo should go
             File photoFile = null;
             try {
@@ -62,10 +55,17 @@ public class MainActivity extends AppCompatActivity {
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                mImageDisplayFragment.setImage(imageBitmap);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageDisplayFragment.setImage(imageBitmap);
         }
     }
 
