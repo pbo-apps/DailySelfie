10b520aa04b622563d3e3c51066b8bf5a272e553
PBO <petebounford@gmail.com>
2016-12-16 13:41:48 +0100

    Allow user to choose initial time for the alarm and use the preferences to drive the reminder settings

--- app/src/main/java/com/pbo/apps/dailyselfie/SelfieReminder.java
+++ app/src/main/java/com/pbo/apps/dailyselfie/SelfieReminder.java
@@ -4,7 +4,9 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.SystemClock;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+import java.util.Calendar;
 
 /**
  * This class handles all interaction with the alarm manager to set up reminders
@@ -12,22 +14,58 @@ import android.os.SystemClock;
 
 class SelfieReminder {
     private final PendingIntent mAlarmIntent;
-
-    private long mAlarmInterval = 15 * 1000;
     private AlarmManager mAlarmMgr;
+    private SharedPreferences mSharedPref;
 
     SelfieReminder(Context context) {
+        context.getSharedPreferences("", Context.MODE_PRIVATE);
         mAlarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, SelfieReminderReceiver.class);
         intent.setAction(MainActivity.EXTERNAL_ACTION_SET_REMINDER);
         mAlarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        mSharedPref = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
     void start() {
-        mAlarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-        SystemClock.elapsedRealtime() + mAlarmInterval,
-        mAlarmInterval,
-        mAlarmIntent);
+        Boolean useReminder = mSharedPref.getBoolean(SettingsFragment.PREF_USE_REMINDER, false);
+        if (useReminder) {
+            long alarmIntervalMillis = getAlarmIntervalMillis();
+            Calendar initialTime = getAlarmInitialTime();
+            if (alarmIntervalMillis > 0) {
+                mAlarmMgr.setInexactRepeating(getAlarmType(),
+                        initialTime.getTimeInMillis(),
+                        alarmIntervalMillis,
+                        mAlarmIntent);
+            } else {
+                mAlarmMgr.set(getAlarmType(),
+                        initialTime.getTimeInMillis(),
+                        mAlarmIntent);
+            }
+        }
+    }
+
+    private long getAlarmIntervalMillis() {
+        String reminderInterval = mSharedPref.getString(SettingsFragment.PREF_REMINDER_INTERVAL, "0");
+        return Long.parseLong(reminderInterval) * 60 * 1000;
+    }
+
+    private Calendar getAlarmInitialTime() {
+        String reminderInitialTime = mSharedPref.getString(SettingsFragment.PREF_REMINDER_INITIAL_TIME, "12");
+        int hourOfDay = Integer.parseInt(reminderInitialTime);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        int currentHourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+        if (hourOfDay <= currentHourOfDay) {
+            calendar.add(Calendar.DATE, 1);
+        }
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        return calendar;
+    }
+
+    private int getAlarmType() {
+        Boolean reminderWakeUp = mSharedPref.getBoolean(SettingsFragment.PREF_REMINDER_WAKE_DEVICE, false);
+        return reminderWakeUp ?
+                AlarmManager.ELAPSED_REALTIME_WAKEUP : AlarmManager.ELAPSED_REALTIME;
     }
 
     void cancel() {
--- app/src/main/java/com/pbo/apps/dailyselfie/SettingsFragment.java
+++ app/src/main/java/com/pbo/apps/dailyselfie/SettingsFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+    public static final String PREF_USE_REMINDER = "pref_reminder";
+    public static final String PREF_REMINDER_WAKE_DEVICE = "pref_reminder_wake_device";
+    public static final String PREF_REMINDER_INTERVAL = "pref_reminder_interval";
+    public static final String PREF_REMINDER_INITIAL_TIME = "pref_reminder_initial_time";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
