1d85f3d1f9a66087c813a1a5148d09eb296a4ed2
PBO <petebounford@gmail.com>
2017-01-16 09:43:12 +0100

    Refactor GalleryItemCursor to use composition rather than inheritance to wrap the Cursor class. This gets the unit test passing because it avoids the need to mock the inherited methods of the CursorWrapper class.

--- app/src/main/java/com/pbo/apps/dailyselfie/GalleryItemCursor.java
+++ app/src/main/java/com/pbo/apps/dailyselfie/GalleryItemCursor.java
@@ -6,32 +6,43 @@ import android.database.CursorWrapper;
 /**
  *  A class to wrap our cursor and reduce calls to getColumnIndex by caching them
  */
-class GalleryItemCursor extends CursorWrapper {
+class GalleryItemCursor {
     private static int CURSOR_INDEX_ID;
     private static int CURSOR_INDEX_PATH;
 
-    GalleryItemCursor(Cursor cursor) {
-        super(cursor);
+    final private Cursor mCursor;
 
-        if (cursor != null) {
-            CURSOR_INDEX_PATH = cursor.getColumnIndex(MainActivity.IMAGE_DATA);
-            CURSOR_INDEX_ID = cursor.getColumnIndex(MainActivity.IMAGE_ID);
+    GalleryItemCursor(Cursor cursor) {
+        mCursor = cursor;
+
+        if (mCursor != null) {
+            CURSOR_INDEX_PATH = mCursor.getColumnIndex(MainActivity.IMAGE_DATA);
+            CURSOR_INDEX_ID = mCursor.getColumnIndex(MainActivity.IMAGE_ID);
         }
     }
 
     String getImagePath() {
-        return getWrappedCursor() == null ?
-                null : ImageFileHelper.getFilePath(getWrappedCursor().getString(CURSOR_INDEX_PATH));
+        return mCursor == null ?
+                null : ImageFileHelper.getFilePath(mCursor.getString(CURSOR_INDEX_PATH));
     }
 
     long getImageID() throws NumberFormatException {
-        return getWrappedCursor() == null ?
-                0 : Long.parseLong(getWrappedCursor().getString(CURSOR_INDEX_ID));
+        return mCursor == null ?
+                0 : Long.parseLong(mCursor.getString(CURSOR_INDEX_ID));
     }
 
-    @Override
-    public int getCount() {
-        return getWrappedCursor() == null ?
-                0 : getWrappedCursor().getCount();
+    int getCount() {
+        return mCursor == null ?
+                0 : mCursor.getCount();
+    }
+
+    Cursor getWrappedCursor() {
+        return mCursor;
+    }
+
+    void moveToPosition(int position) {
+        if (mCursor != null) {
+            mCursor.moveToPosition(position);
+        }
     }
 }
